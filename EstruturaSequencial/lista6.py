666e59abec2c488c1aed3a5bb8f6b17ab3205d7e
Donizete Crisostomo <donizete112@gmail.com>
2023-01-15 17:03:08 -0300

    solucao

---
+++ EstruturaSequencial/lista6.py
@@ -0,0 +1,4 @@
+raio = float(input("Informe o raio: "))
+
+area = 3.14*(raio*raio)
+print("Area eh: {:.2f}".format(area))
---
+++ EstruturaSequencial/lista7.py
@@ -0,0 +1,6 @@
+altura = int(input())
+base = int(input())
+
+area = (altura*base)*2
+
+print("A area eh: ", area)
