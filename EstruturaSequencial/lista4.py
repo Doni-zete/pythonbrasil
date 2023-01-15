8dbe46c935458a8526524fdf684aa0b643fda06a
Donizete Crisostomo <donizete112@gmail.com>
2023-01-15 12:47:28 -0300

    solucao

---
+++ EstruturaSequencial/lista4.py
@@ -0,0 +1,5 @@
+nota1, nota2, nota3, nota4 = list(map(int, input("Entre com as notas: ").split()))
+
+calculo = (nota1 + nota2+nota3+nota4)/4
+
+print("A média eh:", calculo)
---
+++ EstruturaSequencial/lista5.py
@@ -0,0 +1,4 @@
+metros = float(input("Entre com o valo em metros: "))
+converte = metros*100
+
+print("Convertido para centimetros: ", converte)
