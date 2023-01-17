dba2c6a45cdb8e9356b3ca11843655866e200bda
Donizete Crisostomo <donizete112@gmail.com>
2023-01-16 22:13:28 -0300

    solucao

---
+++ EstruturaSequencial/lista18.py
@@ -0,0 +1,6 @@
+arquivo_download = int(input("Informe o tamanho de um arquivo para download: "))
+velocidade_internet = int(input("Informe a velocidade de um link de internet em MBps: "))
+
+calculo = arquivo_download/(velocidade_internet/8)
+
+print("Tempo aproximado de download: ", calculo)
