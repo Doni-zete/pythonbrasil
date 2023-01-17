abf600b06b546b9c2680c473aad6651ceb689971
Donizete Crisostomo <donizete112@gmail.com>
2023-01-16 21:36:04 -0300

    solucao

--- EstruturaSequencial/lista16.py
+++ EstruturaSequencial/lista16.py
@@ -1,12 +1,13 @@
 metros_quadrados = int(input("Informe o metro quadrados a ser pintado: "))
 
 
-if(metros_quadrados > 3):
-    calculo = metros_quadrados/3
-    total = calculo/18
+if(metros_quadrados % 54 == 0):
+    calculo = metros_quadrados/54
 else:
-    if(metros_quadrados < 3):
-        calculo = metros_quadrados/3
-        total = calculo/18
+    calculo = int(metros_quadrados/54) + 1
 
-print(total)
+print(calculo)
+
+preco = calculo * 80
+print('%d latas' % calculo)
+print('R$ %.2f' % preco)
