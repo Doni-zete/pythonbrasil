7cd8914e37df0ea8dc68449839e8b092311923d6
Donizete Crisostomo <donizete112@gmail.com>
2023-01-24 21:39:18 -0300

    solucao

---
+++ EstruturaDeDecisao/lista20.py
@@ -0,0 +1,12 @@
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+
+media = (nota1+nota2+nota3)/3
+
+if(media == 10):
+    print("Aprovado com Distinção")
+elif(media >= 7 and media <= 10):
+    print("Aprovado")
+else:
+    print("Reprovado")
